9a27eb2935f96541577bb1eba3b7e31f0118a539
FauzanFebri08 <242510294@smkn4-tsm.sch.id>
2026-05-20 09:05:57 +0700

    Memperbaiki rute dashboard dan form logout di navbar

--- resources/views/Layouts/navbar.blade.php
+++ resources/views/Layouts/navbar.blade.php
@@ -1,27 +1,30 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-<div class="container-fluid">
+    <div class="container-fluid">
     <a class="navbar-brand" href="#">POS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="{{ route('logout') }}">dashboard</a>
+            <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="{{ route('dashboard') }}">Dashboard</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link {{ Request::is('admin/users') ? 'active' : '' }}" href="{{ route('admin.users') }}">users</a>
+            <a class="nav-link {{ Request::is('admin/users') ? 'active' : '' }}" href="{{ route('admin.users') }}">Users</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link {{ Request::is('produk') ? 'active' : '' }}" href="{{ route('produk.index') }}">produk</a>
+            <a class="nav-link {{ Request::is('produk') ? 'active' : '' }}" href="{{ route('produk.index') }}">Produk</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link {{ Request::is('penjualan') ? 'active' : '' }}" href="{{ route('penjualan.index') }}">penjualan</a>
+            <a class="nav-link {{ Request::is('penjualan') ? 'active' : '' }}" href="{{ route('penjualan.index') }}">Penjualan</a>
         </li>
-    <form class="position-absolute top-50 start-100 translate-middle" action="{{ route('logout')}}" method="POST">
+        </ul>
+    
+        <form class="d-flex" action="{{ route('logout') }}" method="POST">
         @csrf
-        <button type="submit" class="btn btn-danger me-4">logout</button>
-    </form>
-    </div>
+        <button type="submit" class="btn btn-danger me-4">Logout</button>
+        </form>
+        </div>
     </div>
 </nav>
